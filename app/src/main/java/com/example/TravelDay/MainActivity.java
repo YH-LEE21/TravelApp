82926cf72c4f7e15ae3bb0652048bb37b799151b
YH-LEE21 <ksmwy9029@naver.com>
2021-05-26 19:25:41 +0900

    ggg

--- app/src/main/java/com/example/TravelDay/MainActivity.java
+++ app/src/main/java/com/example/TravelDay/MainActivity.java
@@ -3,7 +3,12 @@ package com.example.TravelDay;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager.widget.ViewPager;
 
@@ -14,22 +19,23 @@ import com.google.android.material.tabs.TabLayout;
 public class MainActivity extends AppCompatActivity {
     private TabLayout tabLayout;
     private ViewPager viewPager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setTitle("Travel Day");
         setContentView(R.layout.activity_main);
 
 
-        tabLayout = (TabLayout)findViewById(R.id.tabLayout);
+        tabLayout = (TabLayout) findViewById(R.id.tabLayout);
         tabLayout.addTab(tabLayout.newTab().setText("일정 메모"));
         tabLayout.addTab(tabLayout.newTab().setText("경비 계산"));
         tabLayout.addTab(tabLayout.newTab().setText("구글 맵"));
-        tabLayout.addTab(tabLayout.newTab().setText("프로필"));
 
         viewPager = (ViewPager) findViewById(R.id.viewpager);
 
         //탭페이저어댑터 생성
-        TabPagerAdapter pagerAdapter = new TabPagerAdapter(getSupportFragmentManager(),tabLayout.getTabCount());
+        TabPagerAdapter pagerAdapter = new TabPagerAdapter(getSupportFragmentManager(), tabLayout.getTabCount());
         viewPager.setAdapter(pagerAdapter);
 
         //페이지체인지 리스너 설정
@@ -54,4 +60,28 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.log_out:
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+                return true;
+            case R.id.user_helper:
+                Toast.makeText(this, "고객지원", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.user_peedback:
+                Toast.makeText(this, "피드백", Toast.LENGTH_SHORT).show();
+                return true;
+            default:
+                return false;
+        }
+    }
 }
