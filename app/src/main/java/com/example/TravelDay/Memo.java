436491210e3303c062144c0a42ae71c86d5cf50e
YH-LEE21 <ksmwy9029@naver.com>
2021-05-27 23:43:44 +0900

    tab 1 80% Clear
    일정 메모

---
+++ app/src/main/java/com/example/TravelDay/Memo.java
@@ -0,0 +1,34 @@
+package com.example.TravelDay;
+
+public class Memo {
+    String maintext;//메모
+    String subText;//날짜
+    int  isdone; //완료여부
+    public Memo(String maintext, String subText, int isdone){
+        this.maintext = maintext;
+        this.subText = subText;
+        this.isdone = isdone;
+    }
+
+    public String getMaintext(){
+        return maintext;
+    }
+    public String getSubText() {
+        return subText;
+    }
+
+    public int getIsdone() {
+        return isdone;
+    }
+    public void setMaintext(String maintext){
+        this.maintext=maintext;
+    }
+
+    public void setSubText(String subText) {
+        this.subText = subText;
+    }
+
+    public void setIsdone(int isdone) {
+        this.isdone = isdone;
+    }
+}
--- app/src/main/java/com/example/TravelDay/TabFragment1.java
+++ app/src/main/java/com/example/TravelDay/TabFragment1.java
@@ -1,59 +1,139 @@
 package com.example.TravelDay;
 
-import android.annotation.SuppressLint;
-import android.app.Activity;
 import android.content.Intent;
+import android.content.IntentSender;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.CalendarView;
-import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
+import java.util.ArrayList;
+import java.util.List;
 
 public class TabFragment1 extends Fragment {
-
-    private View view;
-    public CalendarView calendarView;
-    public Button cha_Btn,del_Btn,save_Btn;
-    public TextView diaryTextView,textView2,textView3;
-    public EditText contextEditText;
-
+    RecyclerView recyclerView;
+    RecyclerAdapter recyclerAdapter;
+    RecyclerView.LayoutManager mLayoutManger;
+    List<Memo> memoList;
 
     @Nullable
     @Override
     public  View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        view =  inflater.inflate(R.layout.tab_fragment_1, container,false);
+        View view=  (ViewGroup) inflater.inflate(R.layout.tab_fragment_1, container,false);
 
-        calendarView  = view.findViewById(R.id.calendarView);
-        diaryTextView=view.findViewById(R.id.diaryTextView);
-        save_Btn=view.findViewById(R.id.save_Btn);
-        del_Btn=view.findViewById(R.id.del_Btn);
-        cha_Btn=view.findViewById(R.id.cha_Btn);
-        textView2=view.findViewById(R.id.textView2);
-        textView3=view.findViewById(R.id.textView3);
-        contextEditText=view.findViewById(R.id.contextEditText);
-        calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+
+
+        Button btnAdd = view.findViewById(R.id.btnAdd);
+        memoList = new ArrayList<Memo>();
+
+        recyclerView = view.findViewById(R.id.recyclerview);
+        mLayoutManger = new LinearLayoutManager(getActivity());
+        recyclerView.setLayoutManager(mLayoutManger);
+
+        recyclerAdapter = new RecyclerAdapter(memoList);
+        recyclerView.setAdapter(recyclerAdapter);
+
+        btnAdd.setOnClickListener(new View.OnClickListener(){
             @Override
-            public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
-                diaryTextView.setVisibility(View.VISIBLE);
-                save_Btn.setVisibility(View.VISIBLE);
-                contextEditText.setVisibility(View.VISIBLE);
-                textView2.setVisibility(View.INVISIBLE);
-                cha_Btn.setVisibility(View.INVISIBLE);
-                del_Btn.setVisibility(View.INVISIBLE);
+            public void onClick(View v) {
+                //새로운 메모 작성
+                Intent intent = new Intent(getActivity(),AddActivity.class);
+                startActivityForResult(intent,0);
             }
         });
+
         return view;
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode,Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == 0){
+            String strMain = data.getStringExtra("main");
+            String strSub = data.getStringExtra("sub");
+
+            Memo memo = new Memo(strMain,strSub,0);
+
+            recyclerAdapter.addItem(memo);
+            recyclerAdapter.notifyDataSetChanged();
+        }
+
+    }
+
+    class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.ItemViewHolder>{
+        private List<Memo> listdate;
+
+        public RecyclerAdapter(List<Memo> listdate){
+            this.listdate=listdate;
+        }
+
+
+
+        @NonNull
+        @Override
+        public ItemViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.list_item,parent,false);
+            return new ItemViewHolder(view);
+        }
+
+        @Override
+        public int getItemCount() {
+            return listdate.size();
+        }
+
+        @Override
+        public void onBindViewHolder(RecyclerAdapter.ItemViewHolder holder, int position) {
+            Memo memo = listdate.get(position);
+
+            holder.mainText.setText(memo.getMaintext());
+            holder.subText.setText(memo.getSubText());
+
+            if(memo.getIsdone() == 0){
+                holder.img.setBackgroundColor(Color.LTGRAY);
+            }
+            else{
+                holder.img.setBackgroundColor(Color.GREEN);
+            }
+        }
+
+        void addItem(Memo memo){
+            listdate.add(memo);
+        }
+
+        void remove(int position){
+            listdate.remove(position);
+        }
+
+        class ItemViewHolder extends  RecyclerView.ViewHolder{
+
+            private TextView mainText;
+            private TextView subText;
+            private ImageView img;
+
+
+            public ItemViewHolder(@NonNull View itemView){
+                super(itemView);
+
+                mainText = itemView.findViewById(R.id.item_maintext);
+                subText = itemView.findViewById(R.id.item_subtext);
+                img = itemView.findViewById(R.id.item_image);
+
+            }
+
+
+        }
     }
 
 }
