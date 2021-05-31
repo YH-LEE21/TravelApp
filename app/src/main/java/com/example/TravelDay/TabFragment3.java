bc3d1fc484a243d24eecda432af2da521405c61d
YH-LEE21 <ksmwy9029@naver.com>
2021-05-31 17:40:20 +0900

    map 50%
    map

--- app/src/main/java/com/example/TravelDay/TabFragment3.java
+++ app/src/main/java/com/example/TravelDay/TabFragment3.java
@@ -1,22 +1,42 @@
 package com.example.TravelDay;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
-import com.google.android.gms.maps.OnMapReadyCallback;
+
+import java.util.List;
 
 public class TabFragment3 extends Fragment {
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.tab_fragment_3, container, false);
 
+
         return view;
     }
+
+
+
 }
