c9acb1af48eaa69d18e652ef052e85ab75c7487a
YH-LEE21 <ksmwy9029@naver.com>
2021-06-03 00:56:43 +0900

    스플래시 화면 fadein 애니메이션 적용

--- app/src/main/java/com/example/TravelDay/SplashActivity.java
+++ app/src/main/java/com/example/TravelDay/SplashActivity.java
@@ -4,6 +4,9 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.Window;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -11,21 +14,38 @@ import androidx.appcompat.app.AppCompatActivity;
 
 //스플래쉬 화면
 public class SplashActivity extends AppCompatActivity {
+
+    Animation anim_FadeIn;
+    TextView textView1,textView2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         supportRequestWindowFeature(Window.FEATURE_NO_TITLE);
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_splash);
-        Handler handler = new Handler();
-        handler.postDelayed(new Runnable() {
+
+        textView1 = findViewById(R.id.textView2);
+        textView2 = findViewById(R.id.textView3);
+
+        anim_FadeIn = AnimationUtils.loadAnimation(this,R.anim.anim_splash_fadein);
+        textView1.startAnimation(anim_FadeIn);
+        textView2.startAnimation(anim_FadeIn);
+
+        anim_FadeIn.setAnimationListener(new Animation.AnimationListener() {
             @Override
-            public void run() {
-                Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
-                startActivity(intent);
-                finish();
+            public void onAnimationStart(Animation animation) {
+
             }
-        },5000);
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                startActivity(new Intent(SplashActivity.this,LoginActivity.class));
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
     }
     @Override
     protected void onPause() {
